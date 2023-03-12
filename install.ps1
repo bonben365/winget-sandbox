0d47312cd161d2b67e1677cb857105470ec8c7fd
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-03-12 21:50:40 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -30,6 +30,8 @@ $url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 Add-AppxPackage -Path "$path\Microsoft.UI.Xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx" -ErrorAction:SilentlyContinue | Out-Null
 
 #Download winget and license file
+Write-Host
+Write-Host Installing Windows Package Manager...
 function getLink($match) {
     $uri = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
     $get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
